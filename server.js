13e5d1c8f162d542abd95076182e034baf8a41db
LiamJMiller <liamjmiller.15@gmail.com>
2023-06-15 17:20:41 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
